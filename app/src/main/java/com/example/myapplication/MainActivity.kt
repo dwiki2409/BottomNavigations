03d049abfb9650833945f8a6696648f6e967089b
1217050089 <1217050089@student.uinsgd.ac.id>
2023-10-13 09:12:33 +0700

    add documentation code

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,46 +6,46 @@ import androidx.fragment.app.Fragment
 import com.example.myapplication.Home
 import com.example.myapplication.databinding.ActivityMainBinding
 
+/**
+* The main activity of the application.
+*/
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        replaceFragment(Home())
+override fun onCreate(savedInstanceState: Bundle?) {
+super.onCreate(savedInstanceState)
+binding = ActivityMainBinding.inflate(layoutInflater)
+setContentView(binding.root)
+replaceFragment(Home())
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
+binding.bottomNavigationView.setOnItemSelectedListener {
 
-            when(it.itemId){
+when (it.itemId) {
 
-                R.id.home -> replaceFragment(Home())
-                R.id.profile -> replaceFragment(Profile())
-                R.id.settings -> replaceFragment(Settings())
+R.id.home -> replaceFragment(Home())
+R.id.profile -> replaceFragment(Profile())
+R.id.settings -> replaceFragment(Settings())
 
-                else ->{
+else -> {
+// Handle other menu items if needed.
+}
 
+}
 
+true
+}
+}
 
-                }
-
-            }
-
-            true
-
-        }
-
-
-    }
-
-    private fun replaceFragment(fragment : Fragment){
-
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
-        fragmentTransaction.commit()
-
-
-    }
+/**
+* Replaces the current fragment in the activity's layout with the specified fragment.
+*
+* @param fragment The fragment to be displayed.
+*/
+private fun replaceFragment(fragment: Fragment) {
+val fragmentManager = supportFragmentManager
+val fragmentTransaction = fragmentManager.beginTransaction()
+fragmentTransaction.replace(R.id.frame_layout, fragment)
+fragmentTransaction.commit()
+}
 }
